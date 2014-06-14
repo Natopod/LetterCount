858d91b923aca05c6ab180c538302afb0700d7a0
Natopod <Ndgreene@gmail.com>
2014-06-13 21:50:57 -0400

    updated with documentation

--- src/lettercount/LetterCount.java
+++ src/lettercount/LetterCount.java
@@ -12,35 +12,39 @@ public class LetterCount {
     
     public static void main(String[] args) {
         int spPosition;
-        int wNumber = 0;
+        
         int nStartPos = 0;
         String userString;
-        String msg = "";
+        String msg = "";  //msg variable will build output message to be displayed at the end
         
               
-        userString = JOptionPane.showInputDialog("Please enter a sentence");
+        userString = JOptionPane.showInputDialog("Please enter a sentence");    //take user input
         msg += userString + "\n";
         
-        spPosition = userString.length() - (userString.length() - userString.indexOf(" "));
-        int wLen = spPosition - nStartPos;
+        spPosition = userString.length() - (userString.length() - userString.indexOf(" "));   //determine the location of the first space
+        int wLen = spPosition - nStartPos;  //determine word length
         
+        //tests for the case that a single word is entered and no spaces exist
         if (userString.indexOf(" ") == -1) {
-            System.out.println("The length of the only word in this string is: " + userString.length());
+            JOptionPane.showMessageDialog(null, "The length of the only word in this string is: " + userString.length());
         }
         else {
+            //loop continues as long as there are more spaces in the string
             while(userString.indexOf(" ", nStartPos) != -1) {
                 msg += "The position of the space is " + spPosition + "\n";
                 wLen = spPosition - nStartPos;
-                nStartPos = spPosition + 1;
+                nStartPos = spPosition + 1;  //variable is used to change where the indexOf method searches within the string
                                 
                 spPosition = userString.length() - (userString.length() - (userString.indexOf(" ", nStartPos)));
                 
                 msg += "word length is: " + wLen +"\n";
             }
+            //in the case that the string ends without a space at the end
             if (userString.indexOf(" ") - spPosition != 0) {
                 msg += "The length of the last word is " + (userString.length() - nStartPos) + "\n";
                 
             }
+            //sow created message
             JOptionPane.showMessageDialog(null, msg);
         }
         
